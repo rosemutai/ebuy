385a0ec0cfa40db66a720495d01153f77966b15d
rosemutai <chepngetichrose2030@gmail.com>
2023-04-29 16:40:56 +0300

    Display each categories products

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,46 +3,102 @@ import Products from './Products';
 
 const Categories = () => {
 
-  const [categories, setCategories] = useState([]);
-   const [currentProducts, setCurrentProducts] = useState([])
+  const [currentProducts, setCurrentProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [loading, setLoading] = useState(false); 
 
   useEffect(() => {
-    const getCategories = async () => {
-      const response = await fetch('https://fakestoreapi.com/products/categories');
-      const data = await response.json();
-      console.log(data);
-      setCategories(data);
-    }
-
+    setLoading(true)
      const getAllProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
       console.log(data);
        setCurrentProducts(data);
+       setFilteredProducts(data);
+       setLoading(false)
     }
 
-    getCategories();
     getAllProducts();
 
   }, [])
+
+  const filterData = (category) => {
+    const updatedData = currentProducts.filter((item) => item.category === category);
+    setFilteredProducts(updatedData);
+  }
+
+   const ShowLoading = () => {
+    return (
+      <div className="loading">Loading...</div>
+    )
+  }
+
+  const DisplayProducts = () => {
+    return (
+      <>
+      <div className="categories flex flex-col bg-brownish/70 h-96 px-2 w-[16%]">
+        <div className='h-full'>
+          <button
+            type='button' 
+            className='my-4 font-regular text-white border-transparent hover:text-gray-600 text-lg'
+            onClick={() => setFilteredProducts(currentProducts)}
+          >
+            All
+          </button>
+          <hr />
+        </div>
+
+        <div className='h-full'>
+          <button
+            type='button' 
+            className='my-4 font-regular text-white border-transparent hover:text-gray-600 text-lg'
+            onClick={() => filterData("women's clothing")}
+          >
+            Women&apos;s Clothing
+          </button>
+          <hr />
+        </div>
+
+        <div className='h-full'>
+          <button
+            type='button' 
+            className='my-4 font-regular text-white border-transparent hover:text-gray-600 text-lg'
+            onClick={() => filterData("men's clothing")}
+          >
+            Men&apos;s Clothing
+          </button>
+          <hr />
+        </div>
+
+        <div className='h-full'>
+          <button
+            type='button' 
+            className='my-4 font-regular text-white border-transparent hover:text-gray-600 text-lg'
+            onClick={() => filterData('jewelery')}
+          >
+            Jewelery
+          </button>
+          <hr />
+        </div>
+
+        <div className='h-full'>
+          <button
+            type='button' 
+            className='my-4 font-regular text-white border-transparent hover:text-gray-600 text-lg'
+            onClick={() => filterData('electronics')}
+          >
+            Electronics
+          </button>
+          <hr />
+        </div>
+      </div>
+      <Products items={filteredProducts}/>
+      </>
+    )
+  }
   return (
     <div className="categories-ad py-24 pl-32 pr-12 bg-white min-h-screen w-screen rounded-md flex">
-      <div className="categories flex flex-col bg-brownish h-96 px-2 w-[16%]">
-        {categories.map((category) => (
-          <div key={category} className='h-full'>
-            <a 
-              href='#' 
-              className='my-4 font-regular text-white   hover:text-gray-600 text-lg'
-            >
-              {category}
-            </a>
-            <hr />
-          </div>
-        ))}
-      </div>
-      <div className="ad w-full mr-7 ">
-        <Products items={currentProducts} />
-      </div>
+        {loading ? <ShowLoading /> : <DisplayProducts /> }
     </div>
   )
 }
