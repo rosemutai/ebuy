f872639c9000162127cdb59727e3578024670764
rosemutai <chepngetichrose2030@gmail.com>
2023-04-29 16:44:55 +0300

    Add hover effects to social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { faFacebook, faTwitter, faYoutube, faInstagram } from '@fortawesome/free
 
 const Footer = () => {
   return (
-    <div className="footer pl-32 flex justify-between bg-black/90 py-12 text-white">
+    <div className="footer px-32 flex justify-between bg-black/90 py-12 text-white">
       <div className="column-one">
-        <h5 className="mb-4 font-bold text-brownish">HELP</h5>
+        <h5 className="mb-4 font-bold text-brownish/70">HELP</h5>
         <ul>
           <li className="mb-4" ><a href="#">Help Center</a></li>
           <li className="mb-4" ><a href="#">Contact Us</a></li>
@@ -15,7 +15,7 @@ const Footer = () => {
       </div>
 
       <div className="column-two">
-        <h5 className="mb-4 font-bold text-brownish">ABOUT EBUY</h5>
+        <h5 className="mb-4 font-bold text-brownish/70">ABOUT EBUY</h5>
         <ul>
           <li className="mb-4"><a href="#">About Us</a></li>
           <li className="mb-4"><a href="#">Terms and Conditions</a></li>
@@ -25,20 +25,38 @@ const Footer = () => {
       </div>
 
       <div className="column-one">
-        <h5 className="mb-4 font-bold text-brownish">FOLLOW US</h5>
-        <ul>
+        <h5 className="mb-4 font-bold text-brownish/70">FOLLOW US</h5>
+        <ul className='flex gap-3'>
           <li className="mb-4">
             <a href="#">
-              <FontAwesomeIcon icon={faFacebook} className='text-white' />
+              <FontAwesomeIcon icon={faFacebook} className='text-white
+                hover:text-brownish/70 hover:scale-90 hover:transition-all' />
             </a>
           </li>
-          <li className="mb-4"><a href="#"></a></li>
-          <li className="mb-4"><a href="#"></a></li>
+          <li className="mb-4">
+            <a href="#">
+              <FontAwesomeIcon icon={faTwitter} className='text-white
+                hover:text-brownish/70 hover:scale-90 hover:transition-all' />
+            </a>
+          </li>
+          <li className="mb-4">
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} className='text-white
+                hover:text-brownish/70 hover:scale-90 hover:transition-all' />
+            </a>
+          </li>
+
+           <li className="mb-4">
+            <a href="#">
+              <FontAwesomeIcon icon={faYoutube} className='text-white
+                hover:text-brownish/70 hover:scale-90 hover:transition-all' />
+            </a>
+          </li>
         </ul>
       </div>
 
       <div className="column-one">
-        <h5 className="mb-4 font-bold text-brownish">HELP</h5>
+        <h5 className="mb-4 font-bold text-brownish/70">HELP</h5>
         <ul>
           <li className="mb-4"><a href="#">Help Center</a></li>
           <li className="mb-4"><a href="#">Contact Us</a></li>
