37720cf30a9bb7e3148fe2b097a3f45cc3d799b4
rosemutai <chepngetichrose2030@gmail.com>
2023-04-30 21:56:26 +0300

    Add cart component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import ProductDetails from './components/ProductDetails';
 import Navbar from './components/Navbar';
+import Cart from './components/Cart';
 
 function App() {
 
   return (
-    <div className='w-full min-h-screen bg-white overflow-x-hidden'>
+    <div className="w-full min-h-screen bg-white overflow-x-hidden">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/products/:id' element={<ProductDetails />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products/:id" element={<ProductDetails />}></Route>
+        <Route path="/carts/:id" element={<Cart />}></Route>
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
