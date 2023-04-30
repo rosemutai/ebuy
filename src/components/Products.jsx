d3c46df522dbea33fc126f87593dffdfb92c4030
rosemutai <chepngetichrose2030@gmail.com>
2023-04-30 15:22:21 +0300

    Display product image as a link

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom"
+
 const Products = ({ items }) => {
 
   return (
@@ -7,7 +9,9 @@ const Products = ({ items }) => {
         { items.map((item) => (
           <div key={item.id} className="product-card bg-white h-60 p-2 w-[12rem] border border-gray-300
             rounded-sm hover:shadow hover:shadow-gray-300">
-            <img src={item.image}  className='w-24 h-24 object-contain'/>
+              <Link to={`/products/${item.id}`}>
+                <img src={item.image}  className='w-24 h-24 object-contain'/>
+              </Link>
             <h5 className='product-name text-sm text-gray-600 my-2'>{item.title.substring(0, 16)}...</h5>
             <h4 className=" my-2 text-brownish/70 font-semibold py-1 rounded-lg text-sm
               border-transparent">KSh. {item.price}
