932aa564964bdc74f5b6db293b9027ec4e679d44
rosemutai <chepngetichrose2030@gmail.com>
2023-04-30 15:17:40 +0300

    Display Navbar in all components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import AdBanner from "./AdBanner"
 import Categories from "./Categories"
 import Footer from "./FOoter"
-import Navbar from "./Navbar"
 
 const Home = () => {
   return (
     <div className='w-full'>
-      <Navbar />
       <AdBanner />
       <Categories />
       <Footer />
