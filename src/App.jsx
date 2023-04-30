9496ab51d729c52455435b2bcd3bba4b7e9c8cbd
rosemutai <chepngetichrose2030@gmail.com>
2023-04-30 15:16:47 +0300

    Display Navbar in all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import ProductDetails from './components/ProductDetails';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div className='w-full min-h-screen bg-white overflow-x-hidden'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/products/:id' element={<ProductDetails />}></Route>
