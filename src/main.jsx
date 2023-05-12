4dbd4cff9db4f184a341ac7767097210db3ee43b
rosemutai <chepngetichrose2030@gmail.com>
2023-05-12 17:39:50 +0300

    Configure cart store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+// import { store } from './store.js'
+// import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-     <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+      <BrowserRouter>
         <App />
-      </React.StrictMode>
-  </BrowserRouter>
-)
+      </BrowserRouter>
+    {/* </Provider> */}
+  </React.StrictMode>
+);
