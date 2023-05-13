6a388119a6745195cdfa748e4f636e84d2c8c0b3
rosemutai <chepngetichrose2030@gmail.com>
2023-05-13 21:14:25 +0300

    Add cart route

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import ProductDetails from './components/ProductDetails';
 import Navbar from './components/Navbar';
-import Cart from './components/Cart';
+// import Cart from './components/Cart';
+import Cart from "./routes/Cart";
 
 function App() {
 
   return (
     <div className="w-full min-h-screen bg-white overflow-x-hidden">
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products/:id" element={<ProductDetails />}></Route>
-        <Route path="/carts/:id" element={<Cart />}></Route>
+        {/* <Route path="/carts/:id" element={<Cart />}></Route> */}
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
     </div>
   );
