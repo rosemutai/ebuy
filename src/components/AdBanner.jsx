c99d1d98bc28eae5eb33da145824b39a1ed50fc8
rosemutai <chepngetichrose2030@gmail.com>
2023-05-26 18:13:22 +0300

    Add mobile banner

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -2,12 +2,13 @@ import jacket from '../assets/jacket.png'
 
 const AdBanner = () => {
   return (
-    <div className='top-ad-banner mt-6 w-full text-white bg-brownish/70 flex px-32 justify-center py-8'>
+    <div className='top-ad-banner mt-[14.6rem] md:mt-6 w-full text-white bg-brownish/70 
+    flex px-4 md:px-32 justify-center py-8  '>
       <div className="left-section flex flex-col justify-center">
-        <h2 className="title text-5xl font-bold tracking-wider">
+        <h2 className="title text-2xl md:text-5xl font-bold tracking-normal md:tracking-wider">
           NEW SEASON ARRIVALS
         </h2>
-        <p className="ad-description my-3 text-xl tracking-wide leading-8">
+        <p className="ad-description my-3 text-xs  md:text-xl tracking-normal md:tracking-wide leading-6 md:leading-8">
           CHECK OUT ALL THE TRENDS
         </p>
       </div>
